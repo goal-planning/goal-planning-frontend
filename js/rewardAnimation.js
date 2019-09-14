30f1dbac680b3cc16c054f79c4ac8fca106c4a2f
Greg kearns <kernzerly@me.com>
2019-09-14 14:53:30 -0700

    added canoe move to 8

--- js/rewardAnimation.js
+++ js/rewardAnimation.js
@@ -2,9 +2,10 @@ let canoe = document.getElementById('canoeAnimate');
 let flag = document.getElementById('flagAnimate');
 let pole = document.getElementById('poleAnimate');
 let flagBox = document.getElementById('flagBoxAnimate');
-
-let frameCount = 5;
+let flagNumber = document.getElementById('testNum');
+let frameCount = 7;
 let iAnime = 1;
+let testCounter = 0;
 
 function runRewardAnimation() {
     canoe.className = 'canoe' + iAnime;
@@ -14,7 +15,7 @@ function runRewardAnimation() {
     setTimeout(function () {    //  call a 0.5s setTimeout when the loop is called
         iAnime++;                     //  increment the counter
         if (iAnime <= frameCount) {   //  if the counter < frameCount, call the loop function
-            runRewardAnimation();     //  .. again which will trigger another 
+            runRewardAnimation();     //  .. again which will trigger another
         }                        //  .. setTimeout()
      }, 200)
 };
@@ -25,5 +26,7 @@ function resetAnimation() {
     pole.className = 'pole1';
     flagBox.className = 'flagBox1';
     iAnime = 1;
-}
+    testCounter ++;
+    flagNumber.innerText= testCounter.toString();
 
+}
