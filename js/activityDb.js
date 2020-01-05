c90f1770dcf085ee6b4f1c941c95c20a70ca67f1
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2020-01-04 17:45:37 -0800

    update: add db data template

--- js/activityDb.js
+++ js/activityDb.js
@@ -4,10 +4,11 @@ acitivities = {
     'date-created': 'use date method',
     'due-date': 'pull from user input',
     'category': 'from user selecting: ....',
-//    'status': ''
+    'isCompleted': false,
 //   
 //
 //
 //
 //
 }
+
