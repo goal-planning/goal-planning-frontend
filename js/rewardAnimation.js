5ee5c5eea80235bd25bf697baafa9c607e81e825
Greg kearns <kernzerly@me.com>
2020-02-01 17:33:17 -0800

    analytic template and animation update

--- js/rewardAnimation.js
+++ js/rewardAnimation.js
@@ -5,7 +5,7 @@ let flagBox = document.getElementById('flagBoxAnimate');
 let flagNumber = document.getElementById('testNum');
 let canoePicture = document.getElementById('canoePicture');
 //  increment for each canoe movement frame count should match frame #
-let frameCount = 75;
+let frameCount = 67;
 let iAnime = 1;
 let testCounter = 0;
 //play the animation to loop through frame count
@@ -20,22 +20,25 @@ function runRewardAnimation() {
         iAnime++;                     //  increment the counter
         flagNumber.innerText= iAnime.toString();
         console.log(iAnime.toString());
-        if (iAnime <= 57) {   //  if the counter < frameCount, call the loop function
-            runRewardAnimation();     //  .. again which will trigger another
-        }                        //  .. setTimeout()
-        else if (iAnime == 58)  {
-          canoePicture.src = "/img/canoe-one.png";
-          runRewardAnimation();
-        }
-        else if (iAnime == 59)  {
-          canoePicture.src = "/img/canoe-three.png";
-          runRewardAnimation();
-        }
-        else if (iAnime <=75)  {
-          runRewardAnimation();
-        }
+        // if (iAnime <= 57) {   //  if the counter < frameCount, call the loop function
+        //     runRewardAnimation();     //  .. again which will trigger another
+        // }                        //  .. setTimeout()
+        // else if (iAnime == 58)  {
+        //   canoePicture.src = "/img/canoe-one.png";
+        //   runRewardAnimation();
+        // }
+        // else if (iAnime == 59)  {
+        //   canoePicture.src = "/img/canoe-three.png";
+        //   runRewardAnimation();
+        // }
+        // else if (iAnime <=90)  {
+        //   runRewardAnimation();
+        // }
 
-        else {}
+        // else {}
+        if (iAnime<frameCount){
+          runRewardAnimation();
+        }
      }, 150)
 
      console.log(canoePicture.src);
