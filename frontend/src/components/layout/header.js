bff0189d30b4dcf825871f00876fc7ab7621f12b
Greg Kearns <gregkearns@Gregs-MacBook-Pro.local>
2020-04-04 18:37:46 -0700

    begin to build navbar transparent

--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/nav.css';
 
 export default class header extends Component {
     render() {
         return (
-            <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="/">Goal Planning</Link>
+            <div className="nav-background">
+                <nav className="navbar navbar-expand-lg navbar-light">
+                <Link className="navbar-brand white-font" to="/">Goal Planning</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
