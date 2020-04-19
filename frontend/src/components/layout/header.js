38ef3e190ce4d1b08d861682e1a64b0029f9f07b
Greg Kearns <gregkearns@Gregs-MacBook-Pro.local>
2020-04-18 19:02:07 -0700

    added white font navbar

--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -6,29 +6,31 @@ export default class header extends Component {
     render() {
         return (
             <div className="nav-background">
-                <nav className="navbar navbar-expand-lg navbar-light">
+                <nav className="navbar navbar-expand-lg navbar-dark">
                 <Link className="navbar-brand white-font" to="/">Goal Planning</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <Link className="nav-link" to="/">
+                <div className="collapse navbar-collapse relative-position" id="navbarSupportedContent">
+                    <ul className="navbar-nav relative-position mr-auto">
+                    <li className="nav-item relative-position">
+                        <Link className="nav-link white-font homelink" to="/">
                             Home
                         </Link>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/analytics">
+                    </li>
+                    <li className="nav-item relative-position">
+                        <Link className="nav-link white-font analyticlink" to="/analytics">
                             Analytics
                         </Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/resources">
+                    <li className="nav-item relative-position">
+                        <Link className="nav-link white-font resourcelink" to="/resources">
                             Resources
                         </Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/community">
+                    <li className="nav-item relative-position">
+                        <Link className="nav-link white-font communitylink" to="/community">
                             Community
                         </Link>
                     </li>
