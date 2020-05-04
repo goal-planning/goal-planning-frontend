6ab0494b7387d38d02d601b3bc9b009740173618
snowhero <xionghiroyuki@gmail.com>
2020-05-04 09:36:04 -0700

    working test template

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './styles/home.css';
 
 export default class home extends Component {
     render() {
         return (
             <div>
-                <h1 className="text-center">Homepage</h1>
+                {/* <h1 className="text-center">Homepage</h1> */}
+                <div className="blue-background"></div>
             </div>
         )
     }
--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -7,7 +7,7 @@ export default class header extends Component {
         return (
             <div className="nav-background">
                 <nav className="navbar navbar-expand-lg navbar-dark">
-                <Link className="navbar-brand white-font" to="/">Goal Planning</Link>
+                <Link className="navbar-brand white-font title-underline" to="/">Goal Planning</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
