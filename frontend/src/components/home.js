01d68b260bc3573175b9f2b49384b016011d21af
Greg Kearns <gregkearns@Gregs-MacBook-Pro.local>
2020-05-03 17:47:10 -0700

    zero image template

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './styles/home.css';
 
 export default class home extends Component {
     render() {
         return (
             <div>
-                <h1 className="text-center">Homepage</h1>
+{/* <h1 className="text-center">Homepage</h1>*/}
+              <div className="hero-image">
+              </div>
             </div>
         )
     }
--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -7,30 +7,30 @@ export default class header extends Component {
         return (
             <div className="nav-background">
                 <nav className="navbar navbar-expand-lg navbar-dark">
-                <Link className="navbar-brand white-font" to="/">Goal Planning</Link>
+                <Link className="navbar-brand white-font text-underline" to="/">Goal Planning</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse relative-position" id="navbarSupportedContent">
                     <ul className="navbar-nav relative-position mr-auto">
-                    <li className="nav-item relative-position">
-                        <Link className="nav-link white-font homelink" to="/">
+                    <li className="nav-item relative-position homelink">
+                        <Link className="nav-link white-font" to="/">
                             Home
                         </Link>
                     </li>
-                    <li className="nav-item relative-position">
-                        <Link className="nav-link white-font analyticlink" to="/analytics">
+                    <li className="nav-item relative-position analyticlink">
+                        <Link className="nav-link white-font" to="/analytics">
                             Analytics
                         </Link>
                     </li>
-                    <li className="nav-item relative-position">
-                        <Link className="nav-link white-font resourcelink" to="/resources">
+                    <li className="nav-item relative-position resourcelink">
+                        <Link className="nav-link white-font" to="/resources">
                             Resources
                         </Link>
                     </li>
-                    <li className="nav-item relative-position">
-                        <Link className="nav-link white-font communitylink" to="/community">
+                    <li className="nav-item relative-position communitylink">
+                        <Link className="nav-link white-font" to="/community">
                             Community
                         </Link>
                     </li>
