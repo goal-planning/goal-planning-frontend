2138d300b6796fdf175d7f90ab9e94f4b1152e21
Greg Kearns <gregkearns@Gregs-MacBook-Pro.local>
2020-05-17 19:38:48 -0700

    transparent react navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="relative-position">
       <Router>
         <Header/>
           <Switch>
